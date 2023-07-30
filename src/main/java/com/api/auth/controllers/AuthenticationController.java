9ada7d934209f3ed81197ad78e39948e285d8906
cinthiams <cinbertolo@gmail.com>
2023-07-29 21:03:54 -0300

    adicionando controllers, domain,past dto

---
+++ src/main/java/com/api/auth/controllers/AuthenticationController.java
@@ -0,0 +1,17 @@
+package com.api.auth.controllers;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("auth")
+public class AuthenticationController {
+
+    @PostMapping("/login")
+    private String login(@RequestBody String nome, int senha){
+
+
+    }
+}
